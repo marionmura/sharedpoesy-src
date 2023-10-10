118a1f9fa58e0df0a61c47227436829b1eceead5
steak-osaure <marion.mura090@gmail.com>
2023-10-10 17:59:34 +0200

    correction app.vue auth google

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,17 @@ import PocketBase from "pocketbase";
     <div class="wrapper" id="signOut">
       <div><SignIn msg="User, please sign in !" /></div>
       <label>email: </label><br />
-      <input type="email" required id="email" placeholder="username@domain.tld"
+      <input
+        type="email"
+        required
+        id="email"
+        placeholder="username@domain.tld"
       /><br />
       <label>password: </label><br />
       <input type="password" required id="passwd" /><br />
-      <button v-on:click="register()">Sign Up</button>
-      <button v-on:click="login()">Sign In</button>
-      <button v-on:click="logout()">Sign out</button>
+      <button v-on:click="loginGoogle()">Sign In with Google</button>
+      <button v-on:click="loginGith()">Sign In with Github</button>
+      <button v-on:click="add()">Add</button>
       <p><label id="status"> You are not yet connected </label><br /></p>
     </div>
   </header>
@@ -31,51 +35,44 @@ import PocketBase from "pocketbase";
 </template>
 
 <script>
+var connected = false;
 var pocketbase_ip = "";
-if (import.meta.env.MODE === "production")
-  pocketbase_ip = "http://193.168.146.6:80";
-else pocketbase_ip = "http://127.0.0.1:8090";
+//if (import.meta.env.MODE === "production")
+  pocketbase_ip = "https://shared-poesy.marionmura.fr:443";
+//else pocketbase_ip = "http://127.0.0.1:8090";
 const pb = new PocketBase(pocketbase_ip);
-
+var currentUser;
 export default {
   methods: {
     //this method allows a new user to sign up the system. Once done, the user receives an email
     //asking for account validation. Once the validation made the user is added to the system
-    async login() {
-      await pb
-        .collection("users")
-        .authWithPassword(
-          document.getElementById("email").value,
-          document.getElementById("passwd").value
-        );
-        if (pb.authStore.isValid) {
-          document.getElementById("status").innerHTML = "You are now logged in";
-        }
-    },
-    //this method allows the already registred user to log in the system.
-    async register() {
-      const currentUser = await pb.collection("users").create({
-        email: document.getElementById("email").value,
-        password: document.getElementById("passwd").value,
-        passwordConfirm: document.getElementById("passwd").value,
-        name: "Nouvel utilisateur",
-      });
-      if (currentUser) {
-        document.getElementById("status").innerHTML =
-          "Waiting for your email validation ...";
-        await pb
-          .collection("users")
-          .requestVerification(document.getElementById("email").value);
+    async loginGoogle() {
+      await pb.collection("users").authWithOAuth2({ provider: "google" });
+      if (pb.authStore.isValid) {
+        document.getElementById("status").innerHTML = "You are now logged in";
+        connected = true;
+        currentUser=pb.authStore.model;
       }
     },
-    async logout() {
-
-      await pb
-        .authStore.clear();
-          // currentUser.value = null,
-          document.getElementById("status").innerHTML = "You are now logged out";
-        }
+    async loginGith() {
+      await pb.collection("users").authWithOAuth2({ provider: "github" });
+      if (pb.authStore.isValid) {
+        document.getElementById("status").innerHTML = "You are now logged in";
+        connected = true;
+        currentUser=pb.authStore.model;
+      }
     }
+    /*,
+    async add() {
+      const record = await pb.collection("poems").create({
+        title: "good year",
+        content: "how a nice year",
+        private: false,
+        email:currentUser.email
+      });
+    },*/
+    //this method allows the already registred user to log in the system.
+  },
 };
 </script>
 
@@ -111,7 +108,7 @@ header {
 a,
 .green {
   text-decoration: none;
-  color: rgb(189, 0, 0);
+  color: hsla(160, 100%, 37%, 1);
   transition: 0.4s;
 }
 
